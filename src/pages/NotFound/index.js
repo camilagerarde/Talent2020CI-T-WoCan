811a660f350e14cc7da33a7d48e8400aa3645246
Camila Cunha <millagerarde@gmail.com>
2020-09-08 16:02:43 -0300

    página não encontrado

---
+++ src/pages/NotFound/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function NotFound() {
+  return (
+    <section>
+      <h1>Ops!</h1>
+      <h2>Página não encontrada </h2>
+      <Link title="Voltar para Home" to="/">
+        VOLTAR
+      </Link>
+    </section>
+  );
+}
+
+export default NotFound;
--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,13 @@
 import React from "react";
 import Home from "./pages/Home";
-//import NotFound from "./pages/NotFound";
+import NotFound from "./pages/NotFound";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 const Routes = () => (
   <BrowserRouter>
     <Switch>
       <Route path="/" component={Home} exact />
-      {/* <Route path="*" component={NotFound} /> */}
+      <Route path="*" component={NotFound} />
     </Switch>
   </BrowserRouter>
 );
