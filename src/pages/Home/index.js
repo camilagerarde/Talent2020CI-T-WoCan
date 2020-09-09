a5c48e188c06567ef23c5b1c198885bba038e390
Carol Moscheta <carolina.moscheta@gmail.com>
2020-09-09 16:59:21 -0300

    change tags, change button

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -5,7 +5,7 @@ import '../../assets/scss/Main.scss';
 
 function Home() {
   return (
-    <main>
+    <div>
       <header className='header'>
         <figure className='logo'>
           <img className='header_logo' src={logo} alt='Imagem do logo' />
@@ -17,29 +17,29 @@ function Home() {
           </p>
         </div>
       </header>
-      <body>
+      <main>
         <div className='nav'>
           <Button name='Iniciar trajetória' />
           <Button name='Guia de profissões' />
           <Button name='Mentoria' />
           <Button name='Iniciativas' />
         </div>
-        <div className='about'>
-          <p className='aboutName'>Sobre</p>
-          <p className='aboutText'>
-            <br />
-            Quer estudar né meu bem, mas não sabe por onde começar? Então se
-            joga na nossa aplicação! Em “Iniciar trajetória” voce pode escolher
-            por qual roteiro de estudo quer começar! Está confuso ainda em qual
-            área você se encaixa, da uma olhada nesse “Guia de Profissoes” que a
-            iniciativa Ada.vc preparou! Em “Mentoria” temos um fórum de dúvidas
-            com pessoas ja inseridas no mercado pra ajudar vocês a prosseguir
-            com os estudos. Por fim, temos as “Iniciativas” com informações
-            sobre diversos programas e o que está rolando de eventos. Agora só
-            pegar o café e bons estudos!
-          </p>
-        </div>
-      </body>
+      </main>
+      <section className='about'>
+        <p className='aboutName'>Sobre</p>
+        <p className='aboutText'>
+          <br />
+          Quer estudar né meu bem, mas não sabe por onde começar? Então se joga
+          na nossa aplicação! Em “Iniciar trajetória” voce pode escolher por
+          qual roteiro de estudo quer começar! Está confuso ainda em qual área
+          você se encaixa, da uma olhada nesse “Guia de Profissoes” que a
+          iniciativa Ada.vc preparou! Em “Mentoria” temos um fórum de dúvidas
+          com pessoas ja inseridas no mercado pra ajudar vocês a prosseguir com
+          os estudos. Por fim, temos as “Iniciativas” com informações sobre
+          diversos programas e o que está rolando de eventos. Agora só pegar o
+          café e bons estudos!
+        </p>
+      </section>
       <footer className='footer'>
         <p>
           Feito com{' '}
@@ -53,7 +53,7 @@ function Home() {
           por Amanda, Camila, Carol e Nick!
         </p>
       </footer>
-    </main>
+    </div>
   );
 }
 
