2ea6714d8bc33bd11ea3a9c7ef0dcbd1c2d94e41
Zaine Nascimento <amandazaine@outlook.com>
2020-09-09 17:50:53 -0300

    Terminando as configurações de scss do componente Menu

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import logoname from '../../assets/image/logo2.png';
+import "../../assets/scss/Main.scss";
+import Menu from '../Menu';
 
 const Header = props => {
     
     return (
         <header>
-            
+            <Menu/>
             <img src={logoname} alt="Logo WoCan"></img>
         </header>
     )
--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,11 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import "../../assets/scss/Main.scss";
 
 const Menu = props => {
+
+    const [status, setStatus] = useState(false);
+    
+    const onClickMenu = () => {
+        setStatus(!status)
+        console.log(status)
+    }
+    
     
     return (
-        <div>
+        <>
+            <div id="menu-bar" onClick={onClickMenu} className={ status ? "changeMenuBar" : ""}>
+                
+                    <div id="menu" className={ status ? "change" : ""} >
+                        <div id="bar1" className="bar"></div>
+                        <div id="bar2" className="bar"></div>
+                        <div id="bar3" className="bar"></div>
+                    </div>
+                    <ul id="nav" className={ status ? "change" : "nav"}>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Start</a></li>
+                        <li><a href="#">Front End</a></li>
+                        <li><a href="#">Back End</a></li>
+                        <li><a href="#">DevOps</a></li>
+                    </ul>                
+                
+            </div>
             
-        </div>
+            
+            
+        </>
     )
 }
 
