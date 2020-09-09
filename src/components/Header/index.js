dd8ed32c468bf0d1d46478a822d56e4d581f4e93
Zaine Nascimento <amandazaine@outlook.com>
2020-09-09 12:15:45 -0300

    Iniciando Menu

---
+++ src/components/Header/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logoname from '../../assets/image/logo2.png';
+
+const Header = props => {
+    
+    return (
+        <header>
+            
+            <img src={logoname} alt="Logo WoCan"></img>
+        </header>
+    )
+}
+
+
+export default Header;
