3b062e0acf38da05d402e3b582244d8e620be85b
Camila Cunha <millagerarde@gmail.com>
2020-09-09 18:58:42 -0300

    exclusão breakpoints

--- src/routes.js
+++ src/routes.js
@@ -1,21 +1,21 @@
-import React from "react";
-import Home from "./pages/Home";
-import Start from "./pages/Start";
-import FrontEnd from "./pages/FrontEnd";
-import Back from "./pages/Back";
-import DevOps from "./pages/DevOps";
-import NotFound from "./pages/NotFound";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import React from 'react';
+import Home from './pages/Home';
+import Start from './pages/Start';
+import FrontEnd from './pages/FrontEnd';
+import Back from './pages/Back';
+import DevOps from './pages/DevOps';
+import NotFound from './pages/NotFound';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 const Routes = () => (
   <BrowserRouter>
     <Switch>
-      <Route path="/" component={Home} exact />
-      <Route path="/start" component={Start} exact />
-      <Route path="/frontend" component={FrontEnd} exact />
-      <Route path="/back" component={Back} exact />
-      <Route path="/devops" component={DevOps} exact />
-      <Route path="*" component={NotFound} />
+      <Route path='/' component={Home} exact />
+      <Route path='/start' component={Start} exact />
+      <Route path='/frontend' component={FrontEnd} exact />
+      <Route path='/backend' component={Back} exact />
+      <Route path='/devops' component={DevOps} exact />
+      <Route path='*' component={NotFound} />
     </Switch>
   </BrowserRouter>
 );
