0dff802eeb9a17d8b4766af0bbe7380aabfb9aa4
Camila Cunha <millagerarde@gmail.com>
2020-09-09 19:07:17 -0300

    alteração componente

--- src/components/ButtonFirstSteps/index.js
+++ src/components/ButtonFirstSteps/index.js
@@ -4,7 +4,12 @@ import '../../assets/scss/Main.scss';
 const ButtonFirstSteps = (props) => {
   return (
     <button className='btn'>
-      <a href={props.url} target='_blank' rel='noopener noreferrer'>
+      <a
+        href={props.url}
+        target='_blank'
+        rel='noopener noreferrer'
+        title={props.title}
+      >
         {' '}
         {props.children}{' '}
       </a>
