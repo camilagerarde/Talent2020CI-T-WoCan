6dce7f888f50387ed9bf579f41748587ad1c9708
Camila Cunha <millagerarde@gmail.com>
2020-09-09 19:07:24 -0300

    inclusão de title nos links

--- src/pages/Start/index.js
+++ src/pages/Start/index.js
@@ -13,14 +13,17 @@ const Start = () => {
         <h1>primeiros passos</h1>
         <FirstSteps
           children='lógica de programação'
+          title='Link externo sobre lógica de programação'
           url='https://www.iped.com.br/cursos-gratis/programacao-e-desenvolvimento/curso-rapido/introducao-logica-programacao'
         />
         <FirstSteps
           children='algoritmos'
+          title='Link externo sobre algoritmos'
           url='https://canaldoensino.com.br/blog/curso-online-gratuito-de-algoritmo'
         />
         <FirstSteps
           children='Back-end ou Front-end?'
+          title='Link externo sobre Back-end e Front-end'
           url='https://tableless.github.io/iniciantes/manual/obasico/o-que-front-back.html'
         />
       </section>
@@ -28,7 +31,7 @@ const Start = () => {
         <CardArea
           area='front'
           onClick={() => history.push('/frontend')}
-          title='front-end'
+          title='Front-end'
         />{' '}
         <CardArea
           area='back'
