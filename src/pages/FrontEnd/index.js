c20f8d2a20da47334da6cf69b281cd613ffa118c
Nicole <nicole.franca.goncalves@gmail.com>
2020-09-09 16:21:50 -0300

    Links aparecendo na página de front

--- src/pages/FrontEnd/index.js
+++ src/pages/FrontEnd/index.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import './style.scss'
 import '../../assets/scss/Main.scss';
+import CardLink from '../../components/CardLink/CardLink';
+import firebase from '../../utils/firebase'
 
-function FrontEnd() {
+
+function FrontEnd () {
+  const [ link, setLink ] = useState([]);
+  useEffect (() =>{
+    firebase.firestore().collection('front-end').get().then((querySnapshot) => {
+      let links = []
+      querySnapshot.forEach((doc) =>{links.push(doc.data())});
+      setLink(links)
+    });
+  },[])
   return (
     <div>
-      <h1>WomanCan FRONT</h1>
+      <main className='main-front'>
+        <h1>Front-end</h1>
+          {link.map(eachLink => <CardLink key={eachLink.title} src={eachLink.image} alt={eachLink.title} 
+          title={eachLink.title} href={eachLink.link}/>)}
+      </main>
     </div>
   );
 }
