0ba524c553c088618cee6189c0253c2a194c310c
Nicole <nicole.franca.goncalves@gmail.com>
2020-09-09 19:01:16 -0300

    Página de back completa e alterações no css do front

--- src/pages/Back/index.js
+++ src/pages/Back/index.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import '../../assets/scss/Main.scss';
+import CardLink from '../../components/CardLink/CardLink';
+import firebase from '../../utils/firebase'
+import './style.scss'
 
-function Back() {
+
+function BackEnd () {
+  const [ link, setLink ] = useState([]);
+  useEffect (() =>{
+    firebase.firestore().collection('back-end').get().then((querySnapshot) => {
+      let links = []
+      querySnapshot.forEach((doc) =>{links.push(doc.data())});
+      setLink(links)
+    });
+  },[])
   return (
-    <div>
-      <h1>WomanCan BACK</h1>
-    </div>
+      <main className='main-back'>
+        <h1>Back-end</h1>
+          {link.map(eachLink => <CardLink key={eachLink.title} src={eachLink.image} alt={eachLink.title} 
+          title={eachLink.title} href={eachLink.link}/>)}
+      </main>
   );
 }
 
-export default Back;
+export default BackEnd;
