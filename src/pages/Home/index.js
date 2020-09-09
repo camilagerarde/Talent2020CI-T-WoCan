f73e26d4260907b02612dd8edfae2f20ed55d016
Carol Moscheta <carolina.moscheta@gmail.com>
2020-09-08 22:56:56 -0300

    header image

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
+import logo from '../../image/logo.png';
 
 function Home() {
   return (
     <header className="header">
       <figure className="logo">
-        <img
-          className="header_logo"
-          src="../../image/logo.png"
-          alt="Imagem do logo"
-        ></img>
+        <img className="header_logo" src={logo} alt="Imagem do logo" />
       </figure>
+      <div>
+        <p>
+          Uma iniciativa para informar e direcionar quem deseja entrar no ramo
+          da Tecnologia, e não sabe por onde começar.
+        </p>
+      </div>
       <div></div>
     </header>
   );
