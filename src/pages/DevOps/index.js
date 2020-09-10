04dc4c1f6bea950db8581406c818629ab7fd9c75
Nicole <nicole.franca.goncalves@gmail.com>
2020-09-10 10:48:26 -0300

    CardDev, pagina devops pronta e alterações CardLink

--- src/pages/DevOps/index.js
+++ src/pages/DevOps/index.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import './style.scss'
 import '../../assets/scss/Main.scss';
+import CardDev from '../../components/CardDev/CardDev'
+import firebase from '../../utils/firebase';
 
 function DevOps() {
+  const [ link, setLink ] = useState([]);
+  useEffect (() =>{
+    firebase.firestore().collection('devops').get().then((querySnapshot) => {
+      let links = []
+      querySnapshot.forEach((doc) =>{links.push(doc.data())});
+      setLink(links)
+    });
+  },[])
+
   return (
-    <div>
-      <h1>WomanCan DEV OPS</h1>
-    </div>
+    <main className='main-front'>
+          <h1>DevOps</h1>
+          {link.map(eachLink => <CardDev key={eachLink.title} 
+          title={eachLink.title} href={eachLink.link}/>)}
+      </main>
   );
 }
 
