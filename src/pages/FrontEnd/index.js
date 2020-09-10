781213f56a3f8c546497bfda023f3a2fc78d9da6
Camila Cunha <millagerarde@gmail.com>
2020-09-10 15:20:05 -0300

    header e card title page front

--- src/pages/FrontEnd/index.js
+++ src/pages/FrontEnd/index.js
@@ -1,24 +1,40 @@
-import React, {useEffect, useState} from 'react';
-import './style.scss'
+import React, { useEffect, useState } from 'react';
+import './style.scss';
 import '../../assets/scss/Main.scss';
+import Header from '../../components/Header';
+import CardTitle from '../../components/CardTitle';
 import CardLink from '../../components/CardLink/CardLink';
 import firebase from '../../utils/firebase';
 
-function FrontEnd () {
-  const [ link, setLink ] = useState([]);
-  useEffect (() =>{
-    firebase.firestore().collection('front-end').get().then((querySnapshot) => {
-      let links = []
-      querySnapshot.forEach((doc) =>{links.push(doc.data())});
-      setLink(links)
-    });
-  },[])
+function FrontEnd() {
+  const [link, setLink] = useState([]);
+  useEffect(() => {
+    firebase
+      .firestore()
+      .collection('front-end')
+      .get()
+      .then((querySnapshot) => {
+        let links = [];
+        querySnapshot.forEach((doc) => {
+          links.push(doc.data());
+        });
+        setLink(links);
+      });
+  }, []);
   return (
-      <main className='main-front'>
-          <h1>Front-end</h1>
-          {link.map(eachLink => <CardLink key={eachLink.title} src={eachLink.image} alt={eachLink.title} 
-          title={eachLink.title} href={eachLink.link}/>)}
-      </main>
+    <main className='main-front'>
+      <Header />
+      <CardTitle title='Front-end' img='front' alt='Página Front-end' />
+      {link.map((eachLink) => (
+        <CardLink
+          key={eachLink.title}
+          src={eachLink.image}
+          alt={eachLink.title}
+          title={eachLink.title}
+          href={eachLink.link}
+        />
+      ))}
+    </main>
   );
 }
 
