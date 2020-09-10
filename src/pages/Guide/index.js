d658f3f1b9c2c61f2491ff89ec70fe5c91b4d82c
Carol Moscheta <carolina.moscheta@gmail.com>
2020-09-10 14:51:20 -0300

    page construction

--- src/pages/Guide/index.js
+++ src/pages/Guide/index.js
@@ -1,12 +1,33 @@
 import React from 'react';
+import Header from '../../components/Header';
+import Construction from '../../assets/image/construction.svg';
 import '../../assets/scss/Main.scss';
 
-function Guide() {
+const Guide = () => {
   return (
-    <div>
-      <h1>WomanCan guia</h1>
-    </div>
+    <>
+      <Header />
+      <main className='main'>
+        <div>
+          <h2>Logo teremos novidades!!</h2>
+          <figure className='construction'>
+            <img
+              className='construction-img'
+              src={Construction}
+              alt='página em construção'
+            />
+          </figure>
+        </div>
+        <section className='information-guide'>
+          <h3>
+            Não você não caiu no limbo, em breve teremos uma página fresquinha
+            pra você consultar nosso Guia de profissões e saber pra qual área
+            direcionar o seu talento!
+          </h3>
+        </section>
+      </main>
+    </>
   );
-}
+};
 
 export default Guide;
