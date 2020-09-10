09bf828fd463259b0bb176aa60add1a9f3bd1b42
Zaine Nascimento <amandazaine@outlook.com>
2020-09-09 22:24:43 -0300

    Card para página de iniciativas: estrutura pronta

--- src/components/CardInic/index.js
+++ src/components/CardInic/index.js
@@ -1,12 +1,21 @@
 import React from "react";
 import "../../assets/scss/Main.scss";
 
+
 const CardInic = (props) => {
 
     return(
-        <div>
-            
-        </div>
+        <article className="basic-structure">
+            <p className="forTitle">{props.title}</p>
+            <p>{props.type}</p>
+            <p>{props.when}</p>
+
+            <div id="imageForCard" className="forAlign" >
+                <img src={props.image} alt="Imagem Ilustrativa" class="cover-card"></img>
+            </div>            
+
+            <p id="resume">{props.resume}</p>
+        </article>
     )
 } 
 
