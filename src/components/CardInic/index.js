7a7e9a327816ee2a05bfea896aae0e0b86050d6a
Zaine Nascimento <amandazaine@outlook.com>
2020-09-10 20:35:21 -0300

    Ajustes no Card de iniciativas

--- src/components/CardInic/index.js
+++ src/components/CardInic/index.js
@@ -4,17 +4,21 @@ import "../../assets/scss/Main.scss";
 
 const CardInic = (props) => {
 
+    const redirect = (link) => {
+        window.open(link, '_blank')
+    }
+
     return(
-        <article className="basic-structure">
+        <article className="basic-structure" onClick={() => redirect(props.link)}>
             <p className="forTitle">{props.title}</p>
             <p>{props.type}</p>
             <p>{props.when}</p>
 
             <div id="imageForCard" className="forAlign" >
-                <img src={props.image} alt="Imagem Ilustrativa" class="cover-card"></img>
+                <img src={props.image} alt="Imagem Ilustrativa" className="cover-card"></img>
             </div>            
 
-            <p id="resume">{props.resume} | <a href={props.link} target="_blank" rel="noopener noreferrer" title={props.titleAc}>Clique aqui</a> </p>
+            <p id="resume" className="resumeCardInic">{props.resume}</p>
             
         </article>
     )
