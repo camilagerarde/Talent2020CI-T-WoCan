746c3d1061f10373a5be6c8c502228644f04895d
Nicole <nicole.franca.goncalves@gmail.com>
2020-09-10 10:55:43 -0300

    Alteração no cardDev

--- src/components/CardDev/CardDev.js
+++ src/components/CardDev/CardDev.js
@@ -4,7 +4,7 @@ import './style.scss';
 
 const CardDev = (props) => {
     return (
-        <section className='content-course'>
+        <section className='content-dev'>
             <p className={props.classTitle}>{props.title}</p>
             <p className='txt-link'><a href={props.href} className='go-course'>Ir para o curso</a></p>
         </section>
